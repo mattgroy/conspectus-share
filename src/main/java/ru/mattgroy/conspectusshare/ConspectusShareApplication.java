cabe38a905d17078b7265c4b8b141c55aa1f1acd
Max Ratkov <32338709+mattgroy@users.noreply.github.com>
2021-09-20 13:27:45 +0500

    Spring Boot initial commit

---
+++ src/main/java/ru/mattgroy/conspectusshare/ConspectusShareApplication.java
@@ -0,0 +1,13 @@
+package ru.mattgroy.conspectusshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConspectusShareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConspectusShareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mattgroy/conspectusshare/ConspectusShareApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mattgroy.conspectusshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConspectusShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
