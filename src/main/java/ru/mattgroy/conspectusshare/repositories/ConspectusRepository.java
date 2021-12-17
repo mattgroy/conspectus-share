38947546dff64f72cb5a2e27eba4764afc164436
Max Ratkov <32338709+mattgroy@users.noreply.github.com>
2021-12-17 12:47:46 +0500

    Реализация сервиса поиска по критериям

--- src/main/java/ru/mattgroy/conspectusshare/repositories/ConspectusRepository.java
+++ src/main/java/ru/mattgroy/conspectusshare/repositories/ConspectusRepository.java
@@ -3,5 +3,8 @@ package ru.mattgroy.conspectusshare.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.mattgroy.conspectusshare.models.Conspectus;
 
+import java.util.List;
+
 public interface ConspectusRepository extends JpaRepository<Conspectus, Long> {
+    List<Conspectus> findByNameContainingIgnoreCase(String name);
 }
--- src/main/java/ru/mattgroy/conspectusshare/services/ConspectusServiceImpl.java
+++ src/main/java/ru/mattgroy/conspectusshare/services/ConspectusServiceImpl.java
@@ -3,19 +3,25 @@ package ru.mattgroy.conspectusshare.services;
 import com.sun.istack.NotNull;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.mattgroy.conspectusshare.Dto.FindConspectusDto;
 import ru.mattgroy.conspectusshare.models.Conspectus;
+import ru.mattgroy.conspectusshare.models.Subject;
 import ru.mattgroy.conspectusshare.repositories.ConspectusRepository;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-public class ConspectusServiceImpl implements ConspectusService{
+public class ConspectusServiceImpl implements ConspectusService {
+    private static final String splitRegex = "[[ ]*|[,]*|[\\.]*|[:]*|[/]*|[!]*|[?]*|[+]*]+";
+
     @NonNull
     private ConspectusRepository conspectusRepository;
 
@@ -49,6 +55,32 @@ public class ConspectusServiceImpl implements ConspectusService{
 
     @Override
     public List<Conspectus> find(FindConspectusDto findConspectusDto) {
-        return null;
+        /*
+        var conspectusTemplate = new Conspectus();
+        var subjectTemplate = new Subject();
+        if (findConspectusDto.getSubjectId() != null) subjectTemplate.setId(findConspectusDto.getSubjectId());
+        if (findConspectusDto.getCourse() != null) subjectTemplate.setCourse(findConspectusDto.getCourse());
+        if (findConspectusDto.getYear() != null) subjectTemplate.setYear(findConspectusDto.getYear());
+        conspectusTemplate.setSubject(subjectTemplate);
+        var example = Example.of(
+                conspectusTemplate,
+                ExampleMatcher.matching()
+                        .withIgnoreNullValues()
+                        .withIgnoreCase()
+                        .withStringMatcher(ExampleMatcher.StringMatcher.CONTAINING));
+
+        return conspectusRepository.findAll(example);
+        */
+
+        return conspectusRepository
+//                .findAll(findConspectusDto.getName(), findConspectusDto.getSubjectId(), findConspectusDto.getCourse(), findConspectusDto.getYear());
+                .findByNameContainingIgnoreCase(findConspectusDto.getName())
+                .stream()
+                .filter(conspectus ->
+//                        (findConspectusDto.getName().trim().isEmpty() || Arrays.stream(findConspectusDto.getName().toLowerCase().split(splitRegex)).anyMatch(conspectus.getName().toLowerCase()::contains)) &&
+                        (findConspectusDto.getSubjectId() == null || conspectus.getSubject().getId().equals(findConspectusDto.getSubjectId())) &&
+                        (findConspectusDto.getCourse() == null || conspectus.getSubject().getCourse() == findConspectusDto.getCourse()) &&
+                        (findConspectusDto.getYear() == null || conspectus.getSubject().getYear() == findConspectusDto.getYear()))
+                .collect(Collectors.toList());
     }
 }
